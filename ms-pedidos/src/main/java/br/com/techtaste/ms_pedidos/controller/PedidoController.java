06beea5893653dbc29684aa745e3b16ba6d8f93a
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-12-18 13:57:25 -0300

    aula 04

--- ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/controller/PedidoController.java
+++ ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/controller/PedidoController.java
@@ -3,6 +3,8 @@ package br.com.techtaste.ms_pedidos.controller;
 import br.com.techtaste.ms_pedidos.dto.PedidoRequestDto;
 import br.com.techtaste.ms_pedidos.dto.PedidoResponseDto;
 import br.com.techtaste.ms_pedidos.service.PedidoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -22,8 +24,13 @@ public class PedidoController {
     private PedidoService service;
 
     @PostMapping
+    @CircuitBreaker(name = "verificaAutorizacao", fallbackMethod = "erroAoCadastrarPedido")
     public ResponseEntity<PedidoResponseDto> cadastrarPedido(@RequestBody PedidoRequestDto pedidoDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.cadastrarPedido(pedidoDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.cadastrarPedido(pedidoDto, false));
+    }
+
+    public ResponseEntity<PedidoResponseDto> erroAoCadastrarPedido(@RequestBody @Valid PedidoRequestDto pedidoDto, Exception e) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.cadastrarPedido(pedidoDto, true));
     }
 
     @GetMapping
--- ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/service/PedidoService.java
+++ ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/service/PedidoService.java
@@ -1,10 +1,12 @@
 package br.com.techtaste.ms_pedidos.service;
 
+import br.com.techtaste.ms_pedidos.dto.AutorizacaoDto;
 import br.com.techtaste.ms_pedidos.dto.PedidoRequestDto;
 import br.com.techtaste.ms_pedidos.dto.PedidoResponseDto;
 import br.com.techtaste.ms_pedidos.model.Pedido;
 import br.com.techtaste.ms_pedidos.model.Status;
 import br.com.techtaste.ms_pedidos.repository.PedidoRepository;
+import br.com.techtaste.ms_pedidos.utils.AutorizacaoPagamentoClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,14 @@ import java.util.stream.Collectors;
 @Service
 public class PedidoService {
     private final PedidoRepository repository;
+    private final AutorizacaoPagamentoClient client;
 
-    public PedidoService(PedidoRepository repository) {
+    public PedidoService(PedidoRepository repository, AutorizacaoPagamentoClient client) {
         this.repository = repository;
+        this.client = client;
     }
 
-    public PedidoResponseDto cadastrarPedido(PedidoRequestDto pedidoDto) {
+    public PedidoResponseDto cadastrarPedido(PedidoRequestDto pedidoDto, Boolean comErro) {
         Pedido pedido = new Pedido();
         BeanUtils.copyProperties(pedidoDto, pedido);
         Status status = Status.AGUARDANDO_PAGAMENTO;
@@ -28,9 +32,25 @@ public class PedidoService {
         pedido.setData(LocalDate.now());
         pedido.calcularTotal();
         repository.save(pedido);
+        if (comErro) {
+            status = Status.ERRO_CONSULTA_PGTO;
+        } else {
+            status = obterStatusPagamento(pedido.getId().toString());
+        }
+        pedido.setStatus(status);
+        repository.save(pedido);
         return new PedidoResponseDto(pedido.getId(), pedido.getStatus(),
                 pedido.getCpf(), pedido.getItens(), pedido.getValorTotal(),
                 pedido.getData());
+    }
+
+    private Status obterStatusPagamento(String id) {
+        AutorizacaoDto autorizacao = client.obterAutorizacao(id);
+        if (autorizacao.status().equalsIgnoreCase("autorizado")) {
+            return Status.PREPARANDO;
+        }
+
+        return Status.RECUSADO;
     }
 
     public List<PedidoResponseDto> obterTodos() {
