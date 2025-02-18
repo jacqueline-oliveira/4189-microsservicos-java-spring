4b5033cb7a6077da649988caa314a68a21a1698f
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2025-02-18 00:55:06 -0300

    aula 05

--- ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/service/PedidoService.java
+++ ms-pedidos/src/main/java/br/com/techtaste/ms_pedidos/service/PedidoService.java
@@ -1,10 +1,12 @@
 package br.com.techtaste.ms_pedidos.service;
 
 import br.com.techtaste.ms_pedidos.dto.AutorizacaoDto;
+import br.com.techtaste.ms_pedidos.dto.EmailDto;
 import br.com.techtaste.ms_pedidos.dto.PedidoRequestDto;
 import br.com.techtaste.ms_pedidos.dto.PedidoResponseDto;
 import br.com.techtaste.ms_pedidos.model.Pedido;
 import br.com.techtaste.ms_pedidos.model.Status;
+import br.com.techtaste.ms_pedidos.producer.UsuarioProducer;
 import br.com.techtaste.ms_pedidos.repository.PedidoRepository;
 import br.com.techtaste.ms_pedidos.utils.AutorizacaoPagamentoClient;
 import org.springframework.beans.BeanUtils;
@@ -18,10 +20,12 @@ import java.util.stream.Collectors;
 public class PedidoService {
     private final PedidoRepository repository;
     private final AutorizacaoPagamentoClient client;
+    private final UsuarioProducer producer;
 
-    public PedidoService(PedidoRepository repository, AutorizacaoPagamentoClient client) {
+    public PedidoService(PedidoRepository repository, AutorizacaoPagamentoClient client, UsuarioProducer producer) {
         this.repository = repository;
         this.client = client;
+        this.producer = producer;
     }
 
     public PedidoResponseDto cadastrarPedido(PedidoRequestDto pedidoDto, Boolean comErro) {
@@ -39,6 +43,7 @@ public class PedidoService {
         }
         pedido.setStatus(status);
         repository.save(pedido);
+        producer.enviarEmail(new EmailDto(pedido.getCpf(), pedido.getId().toString(), pedido.getStatus().toString()));
         return new PedidoResponseDto(pedido.getId(), pedido.getStatus(),
                 pedido.getCpf(), pedido.getItens(), pedido.getValorTotal(),
                 pedido.getData());
